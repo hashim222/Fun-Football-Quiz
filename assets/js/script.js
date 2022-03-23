d50b1ebdfdc85aca2a384d5cdac0c72b4456bf6b
hashim222 <aslamhashim19@gmail.com>
2022-03-23 10:04:02 +0000

    changed object to arrays in js files aswell few more things added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,132 +4,141 @@
  let myQuestions = [
   {
     question: "Which country won the first ever World Cup in 1930?",
-    answers: {
-      1: "Australia",
-      2: "England",
-      3: "Spain",
-      4: "Uruguay",
-    },
+    answers: ["Australia", "England", "Spain", "Uruguay"],
     correctAns: 4,
   },
   {
     question:
       "Can you name the former Germany international who went on to become a professional wrestler in the WWE?",
-    answers: {
-      1: "Tim Wiese",
-      2: "Dwayne Johnson",
-      3: "Roman Reigns",
-      4: "Brock Lesnar",
-    },
+    answers: ["Tim Wiese", "Dwayne Johnson", "Roman Reigns", "Brock Lesnar"],
     correctAns: 1,
   },
   {
     question: "Which former Tottenham manager has competed in the Dakar Rally?",
-    answers: {
-      1: "Harry Kane",
-      2: "Hugo Lloris",
-      3: "Andre Villas-Boas",
-      4: "Eric Dier",
-    },
+    answers: [ "Harry Kane", "Hugo Lloris", "Andre Villas-Boas", "Eric Dier",
+    ],
     correctAns: 3,
   },
   {
     question:
       "English rock star Elton John was twice the owner of which football club?",
-    answers: {
-      1: "Watford",
-      2: "Liverpool",
-      3: "Leeds United",
-      4: "Everton",
-    },
+    answers: [
+     "Watford",
+     "Liverpool",
+     "Leeds United",
+     "Everton",
+    ],
     correctAns: 1,
   },
   {
     question:
       "Rangers tried to sign which superstar after Alex McLeish was alerted to his ability through popular video game Football Manager?",
-    answers: {
-      1: "Cristiano Ronaldo",
-      2: "Lionel Messi",
-      3: "Pogba",
-      4: "Eden Hazard",
-    },
+    answers: [
+     "Cristiano Ronaldo",
+     "Lionel Messi",
+     "Pogba",
+     "Eden Hazard",
+    ],
     correctAns: 2,
   },
   {
     question:
       "Which Portuguese team did Ronaldo play for before signing for Manchester United?",
-    answers: {
-      1: "Sporting",
-      2: "FC Barcelona",
-      3: "Manchester United",
-      4: "Manchester City",
-    },
+    answers: [
+      "Sporting",
+      "FC Barcelona",
+      "Manchester United",
+      "Manchester City",
+    ],
     correctAns: 1,
   },
   {
     question: "Who is the Champions League's top goalscorer of all time?",
-    answers: {
-      1: "Mohamed Salah",
-      2: "Neymar Jr",
-      3: "Lionel Messi",
-      4: "Cristiano Ronaldo",
-    },
+    answers: [
+       "Mohamed Salah",
+       "Neymar Jr",
+       "Lionel Messi",
+       "Cristiano Ronaldo",
+],
     correctAns: 4,
   },
   {
     question:
       "Ronaldo helped Portugal win the European Championship in which year?",
-    answers: {
-      1: "2020",
-      2: "2016",
-      3: "2012",
-      4: "2008",
-    },
+    answers: [
+       "2020",
+       "2016",
+       "2012",
+       "2008",
+    ],
     correctAns: 2,
   },
   {
     question:
       "Which German multinational sportswear company is Messi an ambassador for?",
-    answers: {
-      1: "Nike",
-      2: "Gucci",
-      3: "Adidas",
-      4: "Fila",
-    },
+    answers: [
+      "Nike",
+      "Gucci",
+      "Adidas",
+      "Fila",
+    ],
     correctAns: 3,
   },
   {
     question:
       "In which year did the European Championship expand from 16 teams to 24 teams?",
-    answers: {
-      1: "Euro 2016",
-      2: "Euro 2012",
-      3: "World Cup 2014",
-      4: "World Cup 2018",
-    },
+    answers: [
+       "Euro 2016",
+       "Euro 2012",
+       "World Cup 2014",
+       "World Cup 2018",
+    ],
     correctAns: 1,
   },
 ];
-
-document.addEventListener("DOMContentLoaded", function () {
-  // quiz buttons stored in this variable
-  let buttons = document.querySelectorAll(".btns");
-  // looping through all the buttons
-  for (let btn of buttons) {
-    btn.addEventListener("click", function () {
-      console.log(this);
-    });
-  }
-});
 // variables for quiz info btns
 let openHiddenTextBtn = document.querySelector(".btn-for-overlay-page");
 let showHiddenText = document.querySelector("#hidden-popup-text");
 let closeHiddenTextBtn = document.querySelector(".fa-xmark");
 
+// username variable
+let enterUser = document.querySelector("#enter-user");
+let saveUser = document.querySelector("#save-user");
+
+//quiz questions varibale
+const quizQuestionValue = document.querySelector(".quiz-questions");
+//quiz answers varibale
+const quizAnsValue1 = document.querySelector(".btn1");
+const quizAnsValue2 = document.querySelector(".btn2");
+const quizAnsValue3 = document.querySelector(".btn3");
+const quizAnsValue4 = document.querySelector(".btn4");
+
+
+let currantQuestions = 2
+
+function questionsData(){
+  let totalQuestions = myQuestions[currantQuestions]
+  quizQuestionValue.innerHTML = totalQuestions.question
+  quizAnsValue1.innerHTML = totalQuestions.answers[0]
+  quizAnsValue2.innerHTML = totalQuestions.answers[1]
+  quizAnsValue3.innerHTML = totalQuestions.answers[2]
+  quizAnsValue3.innerHTML = totalQuestions.answers[3]
+}
+questionsData()
+
+
+let totalBtns = document.querySelectorAll('.multiple-choices-answers')
+for(let btns of totalBtns){
+  btns.addEventListener('click', function(){
+    currantQuestions++
+    questionsData()
+  })
+}
+
 /**
  * when the user clicks on "quiz info" button, popup page will appear.
  */
-function overlayPage() {
+ function overlayPage() {
   if (showHiddenText.style.display === "block") {
     showHiddenText.style.display = "none";
   } else {
@@ -139,3 +148,39 @@ function overlayPage() {
 openHiddenTextBtn.addEventListener("click", overlayPage);
 // when user wanna exit from quiz info.
 closeHiddenTextBtn.addEventListener("click", overlayPage);
+
+// save the username in quiz.
+function saveUserName() {
+  alert(`tnx! for sigining in, ${enterUser.value}`);
+}
+saveUser.addEventListener("click", saveUserName);
+
+let quizQuestions = document.querySelector(".quiz-questions");
+console.log(quizQuestions);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
