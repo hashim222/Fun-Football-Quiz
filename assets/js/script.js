372f1754621bd96bf9d5e37ba47581be78fb6f6e
hashim222 <aslamhashim19@gmail.com>
2022-03-23 12:07:23 +0000

    changed some of the comments js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /**
- * All the Questions and Answers in this array object
+ * All the Questions and Answers data stored in this array object
  */
  let myQuestions = [
   {
@@ -15,30 +15,19 @@
   },
   {
     question: "Which former Tottenham manager has competed in the Dakar Rally?",
-    answers: [ "Harry Kane", "Hugo Lloris", "Andre Villas-Boas", "Eric Dier",
-    ],
+    answers: ["Harry Kane", "Hugo Lloris", "Andre Villas-Boas", "Eric Dier"],
     correctAns: 3,
   },
   {
     question:
       "English rock star Elton John was twice the owner of which football club?",
-    answers: [
-     "Watford",
-     "Liverpool",
-     "Leeds United",
-     "Everton",
-    ],
+    answers: ["Watford", "Liverpool", "Leeds United", "Everton"],
     correctAns: 1,
   },
   {
     question:
       "Rangers tried to sign which superstar after Alex McLeish was alerted to his ability through popular video game Football Manager?",
-    answers: [
-     "Cristiano Ronaldo",
-     "Lionel Messi",
-     "Pogba",
-     "Eden Hazard",
-    ],
+    answers: ["Cristiano Ronaldo", "Lionel Messi", "Pogba", "Eden Hazard"],
     correctAns: 2,
   },
   {
@@ -55,88 +44,43 @@
   {
     question: "Who is the Champions League's top goalscorer of all time?",
     answers: [
-       "Mohamed Salah",
-       "Neymar Jr",
-       "Lionel Messi",
-       "Cristiano Ronaldo",
-],
+      "Mohamed Salah",
+      "Neymar Jr",
+      "Lionel Messi",
+      "Cristiano Ronaldo",
+    ],
     correctAns: 4,
   },
   {
     question:
       "Ronaldo helped Portugal win the European Championship in which year?",
-    answers: [
-       "2020",
-       "2016",
-       "2012",
-       "2008",
-    ],
+    answers: ["2020", "2016", "2012", "2008"],
     correctAns: 2,
   },
   {
     question:
       "Which German multinational sportswear company is Messi an ambassador for?",
-    answers: [
-      "Nike",
-      "Gucci",
-      "Adidas",
-      "Fila",
-    ],
+    answers: ["Nike", "Gucci", "Adidas", "Fila"],
     correctAns: 3,
   },
   {
     question:
       "In which year did the European Championship expand from 16 teams to 24 teams?",
-    answers: [
-       "Euro 2016",
-       "Euro 2012",
-       "World Cup 2014",
-       "World Cup 2018",
-    ],
+    answers: ["Euro 2016", "Euro 2012", "World Cup 2014", "World Cup 2018"],
     correctAns: 1,
   },
 ];
-// variables for quiz info btns
+// variables for Quiz information buttons
 let openHiddenTextBtn = document.querySelector(".btn-for-overlay-page");
 let showHiddenText = document.querySelector("#hidden-popup-text");
 let closeHiddenTextBtn = document.querySelector(".fa-xmark");
 
-// username variable
+// variable for Username
 let enterUser = document.querySelector("#enter-user");
 let saveUser = document.querySelector("#save-user");
 
-//quiz questions varibale
-const quizQuestionValue = document.querySelector(".quiz-questions");
-//quiz answers varibale
-const quizAnsValue1 = document.querySelector(".btn1");
-const quizAnsValue2 = document.querySelector(".btn2");
-const quizAnsValue3 = document.querySelector(".btn3");
-const quizAnsValue4 = document.querySelector(".btn4");
-
-
-let currantQuestions = 2
-
-function questionsData(){
-  let totalQuestions = myQuestions[currantQuestions]
-  quizQuestionValue.innerHTML = totalQuestions.question
-  quizAnsValue1.innerHTML = totalQuestions.answers[0]
-  quizAnsValue2.innerHTML = totalQuestions.answers[1]
-  quizAnsValue3.innerHTML = totalQuestions.answers[2]
-  quizAnsValue3.innerHTML = totalQuestions.answers[3]
-}
-questionsData()
-
-
-let totalBtns = document.querySelectorAll('.multiple-choices-answers')
-for(let btns of totalBtns){
-  btns.addEventListener('click', function(){
-    currantQuestions++
-    questionsData()
-  })
-}
-
 /**
- * when the user clicks on "quiz info" button, popup page will appear.
+ * This fucntion is for popup page for Quiz information
  */
  function overlayPage() {
   if (showHiddenText.style.display === "block") {
@@ -146,41 +90,49 @@ for(let btns of totalBtns){
   }
 }
 openHiddenTextBtn.addEventListener("click", overlayPage);
-// when user wanna exit from quiz info.
+// This fucntion is for Close popup page for Quiz information
 closeHiddenTextBtn.addEventListener("click", overlayPage);
 
-// save the username in quiz.
+/**
+ *  This fucntion save Username in this Quiz
+ */
 function saveUserName() {
   alert(`tnx! for sigining in, ${enterUser.value}`);
 }
 saveUser.addEventListener("click", saveUserName);
 
-let quizQuestions = document.querySelector(".quiz-questions");
-console.log(quizQuestions);
+// varibale for Quiz Questions
+let quizQuestionValue = document.querySelector(".quiz-questions");
+// varibale for Quiz Answers
+let quizAnsValue1 = document.querySelector(".btn1");
+let quizAnsValue2 = document.querySelector(".btn2");
+let quizAnsValue3 = document.querySelector(".btn3");
+let quizAnsValue4 = document.querySelector(".btn4");
 
+// Questions track
+let currantQuestions = 0;
 
+/**
+ * This function gets innervalue of questions and answers from html
+ */
+function questionsData() {
+  let myQuestionsInShorts = myQuestions[currantQuestions];
+  quizQuestionValue.innerHTML = myQuestionsInShorts.question;
+  quizAnsValue1.innerHTML = myQuestionsInShorts.answers[0];
+  quizAnsValue2.innerHTML = myQuestionsInShorts.answers[1];
+  quizAnsValue3.innerHTML = myQuestionsInShorts.answers[2];
+  quizAnsValue4.innerHTML = myQuestionsInShorts.answers[3];
+}
+questionsData();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let multipleBtnsInQuiz = document.querySelectorAll(".multiple-choices-answers");
+for (let btns of multipleBtnsInQuiz) {
+  btns.addEventListener("click", function () {
+    if (currantQuestions < myQuestions.length) {
+      questionsData();
+    } else {
+      alert("Tnx guys! for answering all the questions in the quiz");
+    }
+    currantQuestions++;
+  });
+}
